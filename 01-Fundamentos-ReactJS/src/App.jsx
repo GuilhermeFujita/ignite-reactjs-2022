2c46cb779419abfb23b3d4429a3ef1a36c6b923e
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-06-04 13:09:20 -0300

    Sidebar component

--- 01-Fundamentos-ReactJS/src/App.jsx
+++ 01-Fundamentos-ReactJS/src/App.jsx
@@ -1,16 +1,23 @@
 import { Header } from './components/Header.jsx';
 import { Post } from './Post.jsx';
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar.jsx';
 
 export function App() {
   return (
     <div className='App'>
       <Header />
-      <Post
-        author='Guilherme Fujita'
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque placeat nulla ad vero beatae, aspernatur, aperiam dicta praesentium voluptatum temporibus qui minima modi cumque explicabo, possimus quam quidem sapiente quasi?'
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Guilherme Fujita'
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque placeat nulla ad vero beatae, aspernatur, aperiam dicta praesentium voluptatum temporibus qui minima modi cumque explicabo, possimus quam quidem sapiente quasi?'
+          />
+        </main>
+      </div>
     </div>
   );
 }
