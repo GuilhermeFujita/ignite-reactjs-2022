001e9637abc74f8e8857df5048f5df1810577044
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-06-04 11:48:44 -0300

    Added components and global css

--- 01-Fundamentos-ReactJS/src/App.jsx
+++ 01-Fundamentos-ReactJS/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import { Header } from './components/Header.jsx';
+import { Post } from './Post.jsx';
 
+import './global.css';
+
+export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className='App'>
+      <Header />
+      <Post
+        author='Guilherme Fujita'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque placeat nulla ad vero beatae, aspernatur, aperiam dicta praesentium voluptatum temporibus qui minima modi cumque explicabo, possimus quam quidem sapiente quasi?'
+      />
     </div>
-  )
+  );
 }
-
-export default App
