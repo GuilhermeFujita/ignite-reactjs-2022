3058453d4e34fc8bdedf8f0bc0f39bb6bc48d7cb
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-07-16 22:56:37 -0300

    Status component

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,31 +20,31 @@ export function History() {
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <Status statusColor="green">Concluido</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <Status statusColor="green">Concluido</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <Status statusColor="green">Concluido</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <Status statusColor="yellow">Em andamento</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>28 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <Status statusColor="red">Interrompido</Status>
             </tr>
           </tbody>
         </table>
