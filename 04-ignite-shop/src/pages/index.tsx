73995caffd421770fe26ce5ccd517fa4089d6fc8
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-09-08 23:34:33 -0300

    Home Structure

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,14 +1,32 @@
-import { styled } from '../styles';
+import Image from 'next/future/image';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-  background: '$green500',
-  borderRadius: 8,
-});
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
 
 export default function Home() {
   return (
     <>
-      <Button>Hello World</Button>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt='' />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image src={camiseta2} width={520} height={480} alt='' />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   );
 }
