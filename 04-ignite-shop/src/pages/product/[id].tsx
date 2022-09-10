fdb9a32f03eaeaa9669b01bd6546f903d6ac61df
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-09-10 16:23:27 -0300

    Product Page Structure

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,11 +1,30 @@
 import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <>
-      <h1>Product: {JSON.stringify(query)}</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer>{/* Imagem */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          provident eum ad! Ad ut possimus expedita ducimus doloribus
+          reprehenderit pariatur, provident inventore maiores, sed est dolore
+          necessitatibus quae rerum repellat?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
