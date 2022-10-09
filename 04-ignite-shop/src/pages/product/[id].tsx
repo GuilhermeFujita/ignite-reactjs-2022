6183fe9bca9e8c51a00f351a7fbe2c9c9fb9be7e
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-10-09 16:52:05 -0300

    Updating fallback property

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -41,7 +41,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [{ params: { id: "prod_MPJ7SaglhWEXWl" } }],
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
