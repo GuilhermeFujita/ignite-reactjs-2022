41bc001bceb7bb7a0179160919ab720d6a9001a0
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-10-09 16:45:49 -0300

    Added dynamic SSG - GetStaticPaths method

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/future/image";
 import { useRouter } from "next/router";
 import Stripe from "stripe";
@@ -37,6 +37,13 @@ export default function Product({ product }: ProductProps) {
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_MPJ7SaglhWEXWl" } }],
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
