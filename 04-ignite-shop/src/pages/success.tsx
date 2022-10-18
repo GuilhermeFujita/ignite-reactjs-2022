e62a12e1f82e983a21e65dc6c716cf5462eee2e0
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-10-17 23:57:41 -0300

    Added Success page

---
+++ 04-ignite-shop/src/pages/success.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
+export default function Success() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Guilherme Fujita</strong>, sua Camiseta{" "}
+        <strong>Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catalogo</Link>
+    </SuccessContainer>
+  );
+}
