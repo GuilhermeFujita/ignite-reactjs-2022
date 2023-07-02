be59c7e1a0a3de76ec5b15381cc409a7effe77fa
GuilhermeFujita <fujitaguilherme@gmail.com>
2023-07-02 15:25:30 -0300

    Added animation on open and close collapsible

--- 07-react-redux-zustand/src/components/Module.tsx
+++ 07-react-redux-zustand/src/components/Module.tsx
@@ -11,7 +11,7 @@ interface ModuleProps {
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   return (
-    <Collapsible.Root>
+    <Collapsible.Root className='group'>
       <Collapsible.Trigger className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
         <div className='flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>
           {moduleIndex + 1}
@@ -22,7 +22,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
           <span className='text-xs text-zinc-400'>{amountOfLessons} aulas</span>
         </div>
 
-        <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+        <ChevronDown className='w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform' />
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className='relative flex flex-col gap-4 p-6'>
